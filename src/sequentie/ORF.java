e1feb72ae0a06a9722f7b6b87a2817810d7c4b01
Ruben <Ruben.kollen@gmail.com>
2018-03-26 14:01:13 +0200

    sequentie half goed

--- src/sequentie/ORF.java
+++ src/sequentie/ORF.java
@@ -3,8 +3,11 @@ import java.util.*;
 public class ORF {
     int n;
     int k;
+    int a = 0;
+    String[] lines = new String [10];
+  
 
-    public void ORFA(String sequentie1, int p){
+    public void ORFA(String sequentie1, int p, boolean check){
      boolean t = false;
         String sequentie2 ="";
      for (int y=p; y+3<sequentie1.length();y+=3){
@@ -18,29 +21,31 @@ public class ORF {
         if (t==true){
         System.out.println("Lengte ingevoerde sequentie:"+sequentie1.length());
 
-        System.out.println("seq2:"+sequentie2);
         int l = sequentie1.length();
-        this.ORFB(sequentie2,sequentie1,l);
+        this.ORFB(sequentie2,sequentie1,l,check);
         
-    }else System.out.println("geen orf gevonden");}
+    }else System.out.println("geen orf gevonden");} 
     
 
-    public void ORFB(String sequentie2,String sequentie1, int l){
+    public void ORFB(String sequentie2,String sequentie1, int l,boolean check){
         k = 0;
         int r = l-sequentie2.length();
         for(int i = 0; i+3<sequentie2.length();i+=3){
             if(k!=i&& sequentie2.substring(i,i+3).contains("TAG")){
                 k = i;
+                a+=1;
                 i+=sequentie2.length();
                 //System.out.println("stopcodon TAG gevonden");
             }
             else if(k!=i && sequentie2.substring(i,i+3).contains("TAA")){
                 k = i;
+                a+=1;
                 i+=sequentie2.length();
                 //System.out.println("stopcodon TAA gevonden");
             }
             else if(k!=i && sequentie2.substring(i,i+3).contains("TGA")){
                 k = i;
+                a+=1;
                 i+=sequentie2.length();
                 //System.out.println("stopcodon TGA gevonden");
             }
@@ -53,21 +58,39 @@ public class ORF {
             int Eindpunt = (k+3+r);
             System.out.println("gevonden reading frame:"+orf + " , locatie:"+Beginpunt +"-"+Eindpunt);
             boolean m = false;
-            String line1="";
+                                                                                            
+                                                                                                
+                                                                                                
+                                                                        
+                                                
+             lines[a]="";
+             
+             if(!check){                       
             for(int b=0;b<Beginpunt;b++){
-                line1+=" ";
-            }
-            line1+=orf;
-            System.out.println("\n"+line1);
-            System.out.println(sequentie1+"\n");
+                for (int a= 0; a<lines.length;a++){
+                lines[a]+=" "; 
+                }}}
+             
+             
+             if(check){
+                for(int b=0;b<(sequentie1.length()-Eindpunt);b++){
+                for (int a= 0; a<lines.length;a++){
+                lines[a]+=" "; 
+                }}}
+             
+                if (check){
+                     String x =new StringBuilder(orf).reverse().toString();
+                     lines[a]+=x;
+                    
+                    }else{
+                    lines[a]+=orf;}
+                           
+               
             for (int j = 0; j+3<sequentie3.length();j+=3){
                 if (m==false && sequentie3.substring(j,j+3).equals("ATG")){
                     m = true;
                     sequentie2 = sequentie3.substring(j);
-                    this.ORFB(sequentie2,sequentie1,l);
-                }
-                else{
-                    //System.out.println("done");
+                    this.ORFB(sequentie2,sequentie1,l,check);
                 }
                
             }
@@ -75,5 +98,11 @@ public class ORF {
         else{
             //System.out.println("geen stop codon gevonden");
         }
+    }
+    public void ORFC(String sequentie1){
+        for (int i = lines.length-1;i>0;i--){
+            System.out.println(lines[i]);
+        }
+        System.out.println(sequentie1);
     }
 }
--- src/sequentie/Sequentie.java
+++ src/sequentie/Sequentie.java
@@ -6,37 +6,43 @@ public class Sequentie {
     String sequentie1;
     int locatie;
     List <ORF> ORFs = new ArrayList<ORF>();
+    boolean check; 
     
     public Sequentie(){
+        check = false;
         sequentie1 = FileOpener.FileOpener();
         System.out.println(sequentie1);
         ORF a= new ORF();
         int p =0;
                 System.out.println("--------------------------------------------------------------------------------------------------------------------");
         System.out.println("\nshift 1:");
-        a.ORFA(sequentie1,p);
+        a.ORFA(sequentie1,p,check);
 
         p+=1;
                 System.out.println("--------------------------------------------------------------------------------------------------------------------");
         System.out.println("\nshift 2:");
-        a.ORFA(sequentie1,p);
+        a.ORFA(sequentie1,p,check);
 
         p+=1;
                 System.out.println("--------------------------------------------------------------------------------------------------------------------");
         System.out.println("\nshift 3:");
-        a.ORFA(sequentie1, p);
+        a.ORFA(sequentie1, p,check);
+        
+        check = true;
         String seq1 =new StringBuilder(sequentie1).reverse().toString();
                 System.out.println("--------------------------------------------------------------------------------------------------------------------");
         System.out.println("\nshift -3:");
-        a.ORFA(seq1, p);
+        a.ORFA(seq1, p,check);
                 System.out.println("--------------------------------------------------------------------------------------------------------------------");
         System.out.println("\nshift -2:");
         p-=1;
-        a.ORFA(seq1, p);
+        a.ORFA(seq1, p,check);
                 System.out.println("--------------------------------------------------------------------------------------------------------------------");
         System.out.println("\nshift -1:");
         p-=1;
-        a.ORFA(seq1, p);
+        a.ORFA(seq1, p,check);
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+        a.ORFC(sequentie1);
         
 
         
