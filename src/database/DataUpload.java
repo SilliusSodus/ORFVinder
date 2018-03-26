fd5fbf69726033d178fc8199e29a6bac671f62e7
E.Verw <everweij@live.nl>
2018-03-26 10:57:59 +0200

    datab testie

--- src/database/DataUpload.java
+++ src/database/DataUpload.java
@@ -1,5 +1,32 @@
 package database;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DataUpload {
 
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        try {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/ORFVinder", "owe7_pg5", "blaat1234");
+        Statement smt = con.createStatement(); //Create Statement to interact
+        ResultSet r = smt.executeQuery("select * from Soort");
+            
+        while (r.next()) {
+          String soort = r.getString("Soort");
+            System.out.println("bier");
+            System.out.println(soort);
+        }
+        con.close();         
+        } catch (com.mysql.jdbc.exceptions.jdbc4.CommunicationsException eex) {
+            Logger.getLogger(DataUpload.class.getName()).log(Level.SEVERE, null, eex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DataUpload.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
+//Queries
+//String Blast_vullen_q = "INSERT INTO BLAST (type,datum,blast_id,sequence_id)"
+
+
