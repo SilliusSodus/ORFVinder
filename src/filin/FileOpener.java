e8b01aa2280e294a1e462d4015f4489479b4cdf9
Ruben <Ruben.kollen@gmail.com>
2018-03-29 15:06:34 +0200

    commentaar

--- src/filin/FileOpener.java
+++ src/filin/FileOpener.java
@@ -6,11 +6,15 @@ public class FileOpener {
 public static String PATHWAY;
 public static String[] sequentie = new String[2];
 
+/**De file open methode wordt gebruikt voor het inlezen van een bestand, en het eruit halen van de sequentie
+ * input is een bestand
+ * output is een sequentie
+ */
 public static String[] FileOpener(){
     JFileChooser fileChooser;
     fileChooser = new JFileChooser();
         File selectedFile;
-        int reply = fileChooser.showOpenDialog(null);                       //openen van bestand + lezen
+        int reply = fileChooser.showOpenDialog(null);                       
         selectedFile = fileChooser.getSelectedFile();
         PATHWAY = selectedFile.toString();
 
